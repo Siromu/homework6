b1426588769eebbd8404f852e9b2b2d1f63213b4
Siro <89258189+Siromu@users.noreply.github.com>
2022-10-16 23:41:41 +0600

    Add files via upload

---
+++ homework6/Class1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO;
+namespace DoubleBinary
+{
+    class Program
+    {
+       public delegate double Func(double x); //создаем делегат такой же сигнатуры, как и функции
+
+        public static double M(double x)
+        {
+            return x * x - 50 * x + 10;
+        }
+
+        public static double D(double x)
+        {
+            return x / x - 50 / x + 10;
+        }
+
+        public static double X(double x)
+        {
+            return x - x + 50 / x - 10;
+        }
+
+        public static void SaveFunc(string fileName, double a, double b, double h, Func F) //добавляем делегат, содержащий функцию
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Create,
+            FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            double x = a;
+            while (x <= b)
+            {
+                bw.Write(F(x)); //F меняется на выбранную нами функцию при вызове через меню, у всех функций присутствует параметр double x
+                x += h;
+            }
+            bw.Close();
+            fs.Close();
+        }
+        public static double Load(string fileName)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader bw = new BinaryReader(fs);
+            double min = double.MaxValue;
+            double d;
+            for (int i = 0; i < fs.Length / sizeof(double); i++)
+            {
+                // Считываем значение и переходим к следующему
+                d = bw.ReadDouble();
+                if (d < min) min = d;
+            }
+            bw.Close();
+            fs.Close();
+            return min;
+        }
+
+    
+        static void Main(string[] args)
+        {
+
+            static void Task(int taskNum = 0) 
+            {
+                bool task = true;
+                while (task) //бесконечный вызов меню 
+                {
+                    Console.WriteLine("Сделайте выбор: 1 - функция M, 2 - функция D, 3 - функция X");
+                    taskNum = int.Parse(Console.ReadLine());
+
+                    switch (taskNum) 
+                    {
+                        case 1:
+                            {
+
+                                SaveFunc("data.bin", -100, 100, 0.5, M);
+                                Console.WriteLine(Load("data.bin"));
+                                break;
+                            }
+                        case 2:
+                            {
+
+                                SaveFunc("data.bin", -100, 100, 0.5, D);
+                                Console.WriteLine(Load("data.bin"));
+                                break;
+
+                            }
+
+                        case 3:
+                            {
+
+                                SaveFunc("data.bin", -100, 100, 0.5, X);
+                                Console.WriteLine(Load("data.bin"));
+                                break;
+
+                            }
+
+                        default:
+                            {
+
+                                Console.WriteLine("Такой задачи нет. Введите корректный номер от 1 до 3");
+                                break;
+                            }
+                            
+                    }
+                }
+            }
+            Task(); //вызов меню
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ homework6/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+// Описываем делегат. В делегате описывается сигнатура методов, на
+// которые он сможет ссылаться в дальнейшем (хранить в себе)
+public delegate double Fun(double x, double y);
+class Program
+{
+    // Создаем метод, который принимает делегат
+    // На практике этот метод сможет принимать любой метод
+    // с такой же сигнатурой, как у делегата
+    public static void Table(Fun F, double x, double y, double b)
+    {
+        Console.WriteLine("----- X ----- Y -----");
+        while (x <= b && y <= b)
+        {
+            Console.WriteLine("| {0,8:0.000} | {1,8:0.000} |", x, F(x, y));
+            x += 1;
+            y += 1;
+        }
+        Console.WriteLine("---------------------");
+    }
+    // Создаем метод для передачи его в качестве параметра в Table
+    public static double MyFunc(double x, double y)
+    {
+        return y * Math.Pow(x, 2);
+    }
+
+    public static double MyFunc2(double x, double y)
+    {
+        return y * Math.Sin(x);
+    }
+
+    static void Main()
+    {
+        // Создаем новый делегат и передаем ссылку на него в метод Table
+        Console.WriteLine("Таблица функции MyFunc:");
+        // Параметры метода и тип возвращаемого значения, должны совпадать с делегатом
+        Table(new Fun(MyFunc), -2, 2, 100);
+
+        Console.WriteLine("Еще раз та же таблица, но вызов организован по новому");
+        // Упрощение(c C# 2.0).Делегат создается автоматически.
+        Table(MyFunc, -2, 2, 100);
+
+        Console.WriteLine("Таблица функции MyFunc2:");
+        Table(MyFunc2, 2, -2, 100); // Можно передавать уже созданные методы
+        Console.WriteLine("Еще раз та же таблица, но вызов организован по новому");
+        // Упрощение(c C# 2.0).Делегат создается автоматически.
+        Table(MyFunc2, -2, 2, 100);
+
+        Console.WriteLine("Таблица функции y*x^2:");
+        // Упрощение(с C# 2.0). Использование анонимного метода
+        Table(delegate (double x, double y) { return y * Math.Pow(x, 2); }, 0, 3, 100);
+        Console.WriteLine("Таблица функции y*sin(x)");
+        // Упрощение(с C# 2.0). Использование анонимного метода
+        Table(delegate (double x, double y) { return y * Math.Sin(x); }, 0, 3, 100);
+    }
+}
